c318b4efe0a75212a9aa22931abb2e7a361ae92b
David Barnett <david@barnett.net.nz>
2014-12-12 11:43:36 +1300

    Working with multiple friends at once
    * Fixed crashes

--- notify.c
+++ notify.c
@@ -1,57 +1,132 @@
 #include "hexchat-plugin.h"
 #include <libnotify/notify.h>
+
+#include <stdlib.h>
 #include <string.h>
 
 #define PNAME "hxnotify"
-#define PDESC "libnotify notifactions for Hexchat"
+#define PDESC "libnotify notifications for Hexchat"
 #define PVERSION "0.1"
+
+//TODO: Support multiple "friends" coming online at once (e.g on-start up)
+//TODO: support channel specific notifications of messages
 
 // plugin handle
 static hexchat_plugin *ph;
 
 // util functions
-char*
-get_username(char* str)
+//Assumes src is pointing at the start of the name
+int get_username_length(char* src)
 {
-	char* ptr = strchr(str, '!');
-	*ptr = '\0'; // ditry trick to use already allocated memory
+	int len;
+	char* itr;
 
-	// Move past the ':' if it at the start of the name
-	if (*str == ':')
-	{
-		str++;
+	len = 0;
+	itr = src;
+
+	while (*itr != '!' && *itr != '\0') {
+		len++;
+		itr++;
 	}
-
-	return str;
+	return len;
 }
 
 // hook functions
-
-static int
-friend_joined_cb (char *word[], char *word_eol[], void *user_data)
+	static int
+friend_online_cb (char* word[], char* word_eol[], void *user_data)
 {
-	NotifyNotification* notify = notify_notification_new("Friend Online",
-			get_username(word[4]), // add more like, " is online"
-			"hexchat");
-	notify_notification_show(notify, NULL);
-	g_object_unref(G_OBJECT(notify));
+	char* friend = strtok(++(word[4]), ","); // pass starting ':'
+	while (friend != NULL) {
+		// TODO: have multiple friends in one message?
+		int nameln = get_username_length(friend);
+		char* msg;
+		size_t msg_len = (nameln + 11) * sizeof(char);
+		msg = malloc(msg_len);
+		memset(msg, '\0', msg_len); //Clean memory
+
+		strncat(msg, friend, nameln);
+		strncat(msg, " is online\0", 11);
+
+		NotifyNotification* notify = notify_notification_new("Friend Online"
+				,msg
+				,"hexchat");
+
+		// FREEDOM!
+		notify_notification_show(notify, NULL);
+		g_object_unref(G_OBJECT(notify));
+		free(msg);
+
+		friend = strtok(NULL, ",");
+	}
+	return HEXCHAT_EAT_NONE;
+}
+
+// TODO: Only show notifications if the friend was already online
+	static int
+friend_offline_cb (char* word[], char* word_eol[], void* user_data)
+{
+	char* friend = strtok(word[4], ",");
+	while (friend != NULL) {
+		int nameln = get_username_length(friend);
+		char* msg;
+		size_t msg_len = (nameln + 11) * sizeof(char);
+		msg = malloc(msg_len);
+		memset(msg, '\0', msg_len); //Clean memory
+
+		strncat(msg, friend, nameln);
+		strcat(msg, " is offline.\0");
+
+		NotifyNotification* notify = notify_notification_new("Friend Offline"
+				,msg
+				,"hexchat");
+
+		notify_notification_show(notify, NULL);
+		g_object_unref(G_OBJECT(notify));
+		free(msg);
+
+		friend = strtok(NULL, ",");
+	}
 
 	return HEXCHAT_EAT_NONE;
 }
 
-static int
+	static int
 notifcation_test(char* word[], char* word_eol[], void* user_data)
 {
-	NotifyNotification* notify = notify_notification_new("Notifcation Test",
-			"Testing", // add more like, " is online"
-			"hexchat");
+	hexchat_printf(ph, "Start of Test\n");
+	char* friend = "friend"; // strtok(word[0], ","); // pass starting ':'
+	hexchat_printf(ph, "Post strtok\n");
+	// TODO: have multiple friends in one message?
+
+	int nameln = get_username_length(friend);
+	hexchat_printf(ph, "Post get_username_length\n");
+
+	char* msg;
+	size_t msg_len = (nameln + 11) * sizeof(char);
+	msg = malloc(msg_len);
+	memset(msg, '\0', msg_len); //Clean memory
+
+	hexchat_printf(ph, "Post malloc\n");
+
+	strcat(msg, friend);
+	hexchat_printf(ph, "msg: %s\n", msg);
+
+	strncat(msg, " is online\0", 11);
+	hexchat_printf(ph, "msg: %s\n", msg);
+
+	NotifyNotification* notify = notify_notification_new("Friend Online"
+			,msg
+			,"hexchat");
+
+	// FREEDOM!
 	notify_notification_show(notify, NULL);
 	g_object_unref(G_OBJECT(notify));
+	free(msg);
 
-	return HEXCHAT_EAT_NONE;
+	return HEXCHAT_EAT_ALL;
 }
 
-int
+	int
 hexchat_plugin_init(
 		hexchat_plugin* plugin_handle,
 		char** plugin_name,
@@ -60,7 +135,7 @@ hexchat_plugin_init(
 		char*  arg)
 {
 	// init libnotify
-	notify_init ("hexchat");
+	notify_init("hexchat");
 
 	// we need to save this for use with any hexchat_* functions
 	ph = plugin_handle;
@@ -69,10 +144,11 @@ hexchat_plugin_init(
 	*plugin_name = PNAME;
 	*plugin_desc = PDESC;
 	*plugin_version = PVERSION;
-	hexchat_printf(ph, "%s plugin loaded successfully!\n", PNAME);
+	hexchat_printf(ph, "%s plugin loaded\n", PNAME);
 
 	// hooks
-	hexchat_hook_server (ph, "730", HEXCHAT_PRI_NORM, friend_joined_cb, NULL);
+	hexchat_hook_server (ph, "730", HEXCHAT_PRI_NORM, friend_online_cb, NULL);
+	//hexchat_hook_server (ph, "731", HEXCHAT_PRI_NORM, friend_offline_cb, NULL);
 
 	//commands
 	hexchat_hook_command(ph, "NotifyTest", HEXCHAT_PRI_NORM, notifcation_test, "", 0);
@@ -80,7 +156,7 @@ hexchat_plugin_init(
 	return 1;
 }
 
-int
+	int
 hexchat_plugin_deinit()
 {
 	// clean up libnotify
