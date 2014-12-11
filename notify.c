95152e2fa5c8bcc7b6bf3f26e6aab7df79eb532b
David Barnett <david@barnett.net.nz>
2014-12-12 11:11:16 +1300

    Test command

--- notify.c
+++ notify.c
@@ -30,16 +30,29 @@ get_username(char* str)
 static int
 friend_joined_cb (char *word[], char *word_eol[], void *user_data)
 {
-	NotifyNotification* Hello = notify_notification_new("Friend Online",
+	NotifyNotification* notify = notify_notification_new("Friend Online",
 			get_username(word[4]), // add more like, " is online"
 			"hexchat");
-	notify_notification_show(Hello, NULL);
-	g_object_unref(G_OBJECT(Hello));
+	notify_notification_show(notify, NULL);
+	g_object_unref(G_OBJECT(notify));
 
 	return HEXCHAT_EAT_NONE;
 }
 
-int hexchat_plugin_init(
+static int
+notifcation_test(char* word[], char* word_eol[], void* user_data)
+{
+	NotifyNotification* notify = notify_notification_new("Notifcation Test",
+			"Testing", // add more like, " is online"
+			"hexchat");
+	notify_notification_show(notify, NULL);
+	g_object_unref(G_OBJECT(notify));
+
+	return HEXCHAT_EAT_NONE;
+}
+
+int
+hexchat_plugin_init(
 		hexchat_plugin* plugin_handle,
 		char** plugin_name,
 		char** plugin_desc,
@@ -61,11 +74,14 @@ int hexchat_plugin_init(
 	// hooks
 	hexchat_hook_server (ph, "730", HEXCHAT_PRI_NORM, friend_joined_cb, NULL);
 
+	//commands
+	hexchat_hook_command(ph, "NotifyTest", HEXCHAT_PRI_NORM, notifcation_test, "", 0);
 	// 1 for success
 	return 1;
 }
 
-int hexchat_plugin_deinit()
+int
+hexchat_plugin_deinit()
 {
 	// clean up libnotify
 	notify_uninit();
