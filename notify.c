bf843eff4afc85532b9f63cfe86d3362b36e8c5a
David Barnett <david@barnett.net.nz>
2014-12-12 13:00:26 +1300

    Get notifications from "blink tray"-enabled channels

--- notify.c
+++ notify.c
@@ -6,16 +6,14 @@
 
 #define PNAME "hxnotify"
 #define PDESC "libnotify notifications for Hexchat"
-#define PVERSION "0.1"
+#define PVERSION "0.2"
 
-//TODO: Support multiple "friends" coming online at once (e.g on-start up)
 //TODO: support channel specific notifications of messages
 
 // plugin handle
 static hexchat_plugin *ph;
 
 // util functions
-//Assumes src is pointing at the start of the name
 int get_username_length(char* src)
 {
 	int len;
@@ -65,7 +63,7 @@ friend_online_cb (char* word[], char* word_eol[], void *user_data)
 	static int
 friend_offline_cb (char* word[], char* word_eol[], void* user_data)
 {
-	char* friend = strtok(word[4], ",");
+	char* friend = strtok(++(word[4]), ",");
 	while (friend != NULL) {
 		int nameln = get_username_length(friend);
 		char* msg;
@@ -126,6 +124,59 @@ notifcation_test(char* word[], char* word_eol[], void* user_data)
 	return HEXCHAT_EAT_ALL;
 }
 
+	static int
+notify_on_channel(char* word[], char* word_eol[], void* user_data)
+{
+	hexchat_list* list = hexchat_list_get(ph, "channels");
+
+	int found = 0;
+	while (hexchat_list_next(ph,list))
+	{
+		hexchat_printf(ph, "Checking %s\n", hexchat_list_str(ph, list, "channel"));
+		if (strcmp(word[3], hexchat_list_str(ph, list, "channel")) == 0) {
+			hexchat_printf(ph, "Message came from %s\n", hexchat_list_str(ph, list, "channel"));
+			found = 1;
+			break;
+		}
+	}
+
+	if (found == 0) {
+		hexchat_list_free(ph, list);
+		return HEXCHAT_EAT_NONE;
+	}
+
+	if ((hexchat_list_int(ph, list, "flag") & 512) == 0) { // 512 is "Blink tray"
+		return HEXCHAT_EAT_NONE;
+	}
+
+	char* friend = word[1];
+	friend++;
+	int nameln = get_username_length(friend);
+	char* mesg = word[4];
+	mesg += 2; // Skip ':+'
+	int mesgln = (strlen(mesg) < 50 ? strlen(mesg) : 50);
+
+	char* msg;
+	size_t msg_len = (nameln + mesgln + 3) * sizeof(char);
+	msg = malloc(msg_len);
+	memset(msg, '\0', msg_len); //Clean memory
+
+	strncat(msg, friend, nameln);
+	strncat(msg, " : ", 3);
+	strncat(msg, mesg, mesgln);
+
+	NotifyNotification* notify = notify_notification_new("Channel Message"
+			,msg
+			,"hexchat");
+
+	// FREEDOM!
+	notify_notification_show(notify, NULL);
+	g_object_unref(G_OBJECT(notify));
+	free(msg);
+	hexchat_list_free(ph, list);
+	return HEXCHAT_EAT_NONE;
+}
+
 	int
 hexchat_plugin_init(
 		hexchat_plugin* plugin_handle,
@@ -148,6 +199,7 @@ hexchat_plugin_init(
 
 	// hooks
 	hexchat_hook_server (ph, "730", HEXCHAT_PRI_NORM, friend_online_cb, NULL);
+	hexchat_hook_server (ph, "PRIVMSG", HEXCHAT_PRI_NORM, notify_on_channel, NULL);
 	//hexchat_hook_server (ph, "731", HEXCHAT_PRI_NORM, friend_offline_cb, NULL);
 
 	//commands
