7464308e5899846296d04e500fbd9f128e957ade
David Barnett <david@barnett.net.nz>
2015-02-25 22:33:45 +1300

    corrected flag

--- hxnotify.py
+++ hxnotify.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
+import hexchat
+
 __module_name__ = "hxnotify"
 __module_version__ = "0.2"
 __module_description__ = "Python module to display tray notifications"
-
-import hexchat
 
 
 def limit_msg(msg, length=50):
@@ -20,8 +20,8 @@ def get_usrname(name):
 
 def friend_online(word, word_eol, userdata):
     for name in word[3][1:].split(','):
-        hexchat.command("tray -b \"Friend Online\" \""
-                        + get_usrname(name) + " is online\"")
+        hexchat.command("tray -b \"Friend Online\" \"" +
+                        get_usrname(name) + " is online\"")
     return None
 
 
@@ -29,9 +29,9 @@ def friend_offline(word, word_eol, userdata):
     for name in word[3].split(','):
         for i in hexchat.get_list("notify"):
             if name == i.nick and i.on != 0:
-                hexchat.command("tray -b \"Friend Offline\" \""
-                                + name
-                                + " has gone offline\"")
+                hexchat.command("tray -b \"Friend Offline\" \"" +
+                                name +
+                                " has gone offline\"")
     return None
 
 
@@ -40,11 +40,11 @@ def channel_msg(word, word_eol, userdata):
         return None
 
     for ch in hexchat.get_list("channels"):
-        if word[2] == ch.channel and bool(ch.flags & 1 << 8):
-            hexchat.command("tray -b \"Message in " + ch.channel + "\" \""
-                            + get_usrname(word[0]) + ": "
-                            + limit_msg(word_eol[3][2:])
-                            + "\"")
+        if word[2] == ch.channel and bool(ch.flags & (1 << 9)):
+            hexchat.command("tray -b \"Message in " + ch.channel + "\" \"" +
+                            get_usrname(word[0]) + ": " +
+                            limit_msg(word_eol[3][2:]) +
+                            "\"")
             break
     return None
 
