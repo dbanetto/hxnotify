66d205a7dea55853fae8a9c60467240588730382
David Barnett <david@barnett.net.nz>
2015-12-01 09:18:21 +1300

    Clean up python & fix quote issue
    Updated readme

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # HXNotify
 
-HexChat C plugin to push notifications to through libnotify
+HexChat C & python plugin to push notifications to through libnotify
 
 Currently pushes a notification for:
 
 * when a friend is online
+* when a channel marked with `alert_tray`, set via `/CHANOPT`
 
 Planned notifications:
 
-* On tray icon blink
 * name is said in a channel
 
 ## Requirements
@@ -16,9 +16,21 @@ Planned notifications:
 * libnotify
 * hexchat
 
+## Note
+
+The python is more stable than the C version.
+
+The C version is prone to crash for unknown reasons, if you see anything wrong
+PRs will be helpful.
+
 ## Build
 
-`make`
+For the C plugin run `make`
+
+## Install
+
+For the C plugin run `make install` to copy the plugin
+or run `make install-link` to symlink the binary.
 
 ## How to use
 
--- hxnotify.py
+++ hxnotify.py
@@ -2,11 +2,13 @@
 import hexchat
 
 __module_name__ = "hxnotify"
-__module_version__ = "0.3"
+__module_version__ = "0.4"
 __module_description__ = "Python module to display tray notifications"
 
 
 def limit_msg(msg, length=160):
+    # fixes issue with quotes cannot be esacped in hexchat commands
+    msg = msg.replace('"', "'")
     if len(msg) > length:
         return msg[0:length - 3] + "..."
     else:
@@ -20,7 +22,8 @@ def get_usrname(name):
 def friend_online(word, word_eol, userdata):
     for name in word[3][1:].split(','):
         hexchat.command("tray -b \"Friend Online\" \"" +
-                        get_usrname(name) + " is online\"")
+                        get_usrname(name) +
+                        " is online\"")
     return None
 
 
@@ -28,7 +31,7 @@ def friend_offline(word, word_eol, userdata):
     for i in hexchat.get_list("notify"):
         for name in word[3][1:].split(','):
             if name == i.nick:
-                if i.on != 0: # check if they have been online
+                if i.on != 0:  # check if they have been online
                     hexchat.command("tray -b \"Friend Offline\" \"" +
                                     name +
                                     " has gone offline\"")
@@ -45,10 +48,10 @@ def channel_msg(word, word_eol, userdata):
             continue
 
         if word[2] == ch.channel:
-            if bool(ch.flags & (1 << 10) != 0): # blink task tray
-                hexchat.command("tray -b \"Message in " + ch.channel + "\" \"" +
-                                get_usrname(word[0]) + ": " +
-                                limit_msg(word_eol[3][2:]) +
+            if bool(ch.flags & (1 << 10) != 0):  # blink task tray
+                hexchat.command("tray -b \"Message in " + ch.channel +
+                                "\" \"" + get_usrname(word[0]) + ": " +
+                                limit_msg(word_eol[3]) +
                                 "\"")
             break
 
