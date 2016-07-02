9ae705bbb8b888ba6566c2c50d78d50461060f7c
David Barnett <david@barnett.net.nz>
2016-07-02 20:53:54 +1200

    plugin revamp
    uses plugin preference to track which channels to show notifications for
    and exposes command hxnotify to manipulate the whitelist
    dropped C version of the plugin all together

--- README.md
+++ README.md
@@ -1,41 +1,42 @@
-# HXNotify
+# hxnotify
 
-HexChat C & python plugin to push notifications to through libnotify
-
-Currently pushes a notification for:
-
-* when a friend is online
-* when a channel marked with `alert_tray`, set via `/CHANOPT`
-
-Planned notifications:
-
-* name is said in a channel
-
-## Requirements
-
-* libnotify
-* hexchat
-
-## Note
-
-The python is more stable than the C version.
-
-The C version is prone to crash for unknown reasons, if you see anything wrong
-PRs will be helpful.
-
-## Build
-
-For the C plugin run `make`
+A python 3 plugin to control OS notification on message in a channel and
+to notification when users you have `/notify`'d login in or off.
 
 ## Install
-
-For the C plugin run `make install` to copy the plugin
-or run `make install-link` to symlink the binary.
-
-## How to use
 
 Either load it manually in HexChat
 or copy it to the add-on folder in :
 
 * Windows `%appdata%\HexChat\Addons`
 * Unix `~/.config/hexchat/addons`
+
+## How to use
+
+The plugin exposes a command `/hxnotify` which has 3 commands for the white list
+of channels that the notification should be for.
+
+* `add` - which adds the listed channels after it, or your current channel if none are provided
+* `rm` - which removes the listed channels after it, or your current channel if none are provided
+* `list` - lists all the channels that are white listed
+
+```
+/hxnotify - configuration control for hxnotify plugin
+/hxnotify add (#channel) - adds channel(s) to whitelist of channels to notify on, defaults to current channel
+/hxnotify rm (#channel)  - removes channel(s) from whitelist of channels to notify on, defaults to current channel
+/hxnotify list - lists channels in whitelist
+/hxnotify test - sends test notification
+```
+
+## Limitations
+
+The whitelist is global and is applied to all networks, so if you have 
+one channel named the same on two networks and only want one of them to be
+whitelisted, this is not supported yet.
+
+This was developed using the Python 3 plugin for HexChat 2.12 on Linux,
+testing on other platforms or earlier versions of python or hexchat are supported
+
+### License
+
+MIT see LICENSE.md for more
--- hxnotify.py
+++ hxnotify.py
@@ -2,17 +2,13 @@
 import hexchat
 
 __module_name__ = "hxnotify"
-__module_version__ = "0.5"
+__module_version__ = "0.5.0"
 __module_description__ = "Python module to display tray notifications"
 
+MESSAGE_LIMIT = 160
 
-def limit_msg(msg, length=160):
-    # fixes issue with quotes cannot be esacped in hexchat commands
-    msg = msg.replace('"', "'")
-    if len(msg) > length:
-        return msg[0:length - 3] + "..."
-    else:
-        return msg
+blink_tray = 1 << 9
+blink_task_bar = 1 << 10
 
 
 def get_usrname(name):
@@ -21,46 +17,149 @@ def get_usrname(name):
 
 def friend_online(word, word_eol, userdata):
     for name in word[3][1:].split(','):
-        hexchat.command("tray -b \"Friend Online\" \"" +
-                        get_usrname(name) +
-                        " is online\"")
+        notification("Friend Online",
+                     "{0} has is online".format(name))
     return None
 
 
 def friend_offline(word, word_eol, userdata):
+    names = word[3][1:].split(',')
     for i in hexchat.get_list("notify"):
-        for name in word[3][1:].split(','):
-            if name == i.nick:
-                if i.on != 0:  # check if they have been online
-                    hexchat.command("tray -b \"Friend Offline\" \"" +
-                                    name +
-                                    " has gone offline\"")
+        if i.nick in names and i.on != 0:
+            notification("Friend Offline",
+                         "{0} has gone offline".format(name))
+
     return None
 
 
 def channel_msg(word, word_eol, userdata):
-    if hexchat.get_info("win_status") == "active":
-        return None
+    # only show when not focused on hexchat
+    # if hexchat.get_info("win_status") == "active":
+    #     return None
 
+    # get the channel info for where the message came from
     for ch in hexchat.get_list("channels"):
         # handle only channel
         if ch.type != 2:
             continue
 
-        if word[2] == ch.channel:
-            if bool(ch.flags & (1 << 10) != 0):  # blink task tray
-                from_user = get_usrname(word[0])
-                # fix getting notifcations from self when a bouncer playback is running
-                if hexchat.get_info('nick').strip() != from_user.strip():
-                    hexchat.command("tray -b \"Message in " + ch.channel +
-                                    "\" \"" + from_user + ": " +
-                                    limit_msg(word_eol[3]) +
-                                    "\"")
-            break
+        whitelist = hexchat.get_pluginpref('hxnotify_whitelist').split(',')
+
+        # only show for whitelisted channels
+        if word[2] in whitelist:
+
+            # check if the chanopt is set
+            if bool(ch.flags & blink_tray):
+                my_nick = hexchat.get_info('nick')
+                from_user = word[0]
+
+                # fix getting notifcations from self when
+                # a bouncer playback is running
+                if hexchat.nickcmp(from_user, my_nick) != 0:
+                    header = "Message ({0}/{1})".format(ch.network, ch.channel)
+                    body = "{0}: {1}".format(get_usrname(from_user),
+                                             hexchat.strip(word_eol[3][1:],
+                                                           MESSAGE_LIMIT, 3))
+                    # send the notification
+                    notification(header, body)
+
+            return None
 
     return None
 
 
+def notification(header, body):
+    hexchat.command('tray -b "{0}" {1}'.format(header, body))
+
+
+HXNOTIFY_HELP = """/hxnotify - configuration control for hxnotify plugin
+Commands
+/hxnotify add (#channel) - adds channel(s) to whitelist of channels to notify on, defaults to current channel
+/hxnotify rm (#channel)  - removes channel(s) from whitelist of channels to notify on, defaults to current channel
+/hxnotify list - lists channels in whitelist
+/hxnotify test - sends test notification
+"""
+
+def hxnotify_cb(word, word_eol, userdata):
+    if len(word) < 2:
+        print('No arguments passed')
+        print(HXNOTIFY_HELP)
+
+    cmd = word[1]
+
+    if cmd == 'add':
+        hxnotify_add(word)
+    elif cmd == 'rm':
+        hxnotify_rm(word)
+    elif cmd == 'list':
+        whitelist = get_whitelist()
+
+        if len(whitelist) < 1:
+            print("No channels whitelisted")
+        else:
+            print("whitelisted channels: {0}"
+                  .format(str.join(', ', whitelist)))
+    elif cmd == 'test':
+        notification('Test', 'testing, 1, 2, 3')
+
+    return hexchat.EAT_ALL
+
+
+def hxnotify_add(word):
+    if len(word) < 3:
+        channels = [hexchat.get_info('channel')]
+    else:
+        channels = word[2:]
+
+    whitelist = get_whitelist()
+
+    for channel in channels:
+        if channel in whitelist:
+            print('{0} is already whitelisted'.format(channel))
+            continue
+
+        whitelist.append(channel)
+        print('{0} has been added to the whitelisted'.format(channel))
+
+    set_whitelist(whitelist)
+
+
+def hxnotify_rm(word):
+    if len(word) < 3:
+        channels = [hexchat.get_info('channel')]
+    else:
+        channels = word[2:]
+
+    for channel in channels:
+        whitelist = get_whitelist()
+
+        if channel not in whitelist:
+            print('{0} is not whitelisted'.format(channel))
+            continue
+
+        whitelist.remove(channel)
+        print('{0} has been removed from the whitelist'.format(channel))
+
+    set_whitelist(whitelist)
+
+
+def get_whitelist():
+    whitelist = hexchat.get_pluginpref('hxnotify_whitelist')
+
+    # ensure we got something back
+    if whitelist is None:
+        return []
+
+    # remove blanks
+    return [w for w in whitelist.split(',') if w.strip()]
+
+
+def set_whitelist(whitelist):
+    # remove blanks
+    whitelist = [w for w in whitelist if w.strip()]
+    hexchat.set_pluginpref('hxnotify_whitelist', str.join(',', whitelist))
+
+hexchat.hook_command("hxnotify", hxnotify_cb, help=HXNOTIFY_HELP)
 hexchat.hook_server("730", friend_online)
 hexchat.hook_server("731", friend_offline)
 hexchat.hook_server("PRIVMSG", channel_msg)
